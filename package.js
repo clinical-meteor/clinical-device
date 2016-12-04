970c950956eb7908d4dd0bbc8e6cc90413f583b5
Abigail Watson <awatson1978@gmail.com>
2016-12-03 20:54:33 -0600

    METEOR@1.4.2.3

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: "clinical:device",
   summary: "Device detection for ClinicalFramework apps.",
-  version: "1.2.0",
+  version: "1.3.0",
   git: "https://github.com/clinical-meteor/device.git"
 });
 
@@ -9,16 +9,12 @@ Package.on_use(function (api) {
   api.versionsFrom('METEOR@1.1.0.3');
 
   api.use([
-    'meteor-platform',
     'underscore',
-    'tracker',
-    'session',
-    'templating'
+    'session'
   ], 'client');
 
-  api.add_files([
-    'client/device_detection.js',
-    'client/device_helpers.js'
+  api.addFiles([
+    'client/device_detection.js'
   ], 'client');
 
   api.export("Device");
